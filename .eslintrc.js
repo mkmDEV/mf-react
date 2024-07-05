cecd580603713c38a902c0b3b026f8cfdd4f691c
Kristof Marsi <marsi.kristof@biotechusa.com>
2024-07-05 13:39:38 +0200

    ADD
        - container and marketing apps are initially implemented

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    camelcase: 0,
+    'comma-dangle': [1, 'only-multiline'],
+  },
+};
---
+++ container/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MarketingApp from './components/MarketingApp';
+
+const App = () => (
+  <>
+    <h1>Hello World</h1>
+    <hr />
+    <MarketingApp />
+  </>
+);
+
+export default App;
